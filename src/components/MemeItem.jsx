c3d88b949d337213c815f93d2fddf9bd5970ef4c
Alvaro Ramos <alvaroramosandler@hotmail.com>
2024-12-17 17:27:41 -0300

    Updated Hovered

--- src/components/MemeItem.jsx
+++ src/components/MemeItem.jsx
@@ -1,46 +1,51 @@
+import { useState } from "react";
 import { Card, Image, Group, Text } from "@mantine/core";
 
 // Cards para mostrar los memes en la pantalla principal
-function MemeItem({ memes, hovered }) {
-    return (
-        <Card
-         withBorder
-         shadow="sm"
-         padding="lg"
-         radius="md"
-         key={memes.id}
-         style={{
-            position: hovered ? "absolute" : "relative",
-            top: hovered ? "-10px" : "0",
-            left: hovered ? "-10px" : "0",
-            zIndex: hovered ? 10 : 1,
-            transition: "transform 0.2s ease, top 0.2s ease, left 0.2s ease",
-            transform: hovered ? "scale(1.2)" : "scale(1)",
-            width: hovered ? "240px" : "100%",
-            height: hovered ? "auto" : "100%",
-            overflow: "hidden",
-         }}
-        > 
-         <Card.Section>
-            <Image 
-              src={memes.img_url} 
-              style={{
-                width: "100%",
-                height: "100%",
-                objectFit: hovered ? "contain" : "cover",
-                transition: "object-fit 0.2s ease",
-              }}
-            />
-         </Card.Section>
-         {hovered && (
-          <Group justify="space-between" mt="md" mb="xs" style={{ flexGrow: 1 }}>
-           <Text fw={500} style={{ width: "60%" }}>
+function MemeItem({ memes }) {
+  const [hovered, setHovered] = useState(false);
+
+  return (
+    <Card
+      withBorder
+      shadow="sm"
+      padding="lg"
+      radius="md"
+      key={memes.id}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      style={{
+        zIndex: hovered ? 10 : 1,
+        transition: "transform 0.2s ease",
+        transform: hovered ? "scale(1.3)" : "scale(1)",
+        width: hovered ? "300px" : "100%",
+        height: hovered ? "auto" : "100%",
+        overflow: "hidden",
+      }}
+    >
+      <Card.Section>
+        <Image
+          src={memes.img_url}
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: hovered ? "contain" : "cover",
+            transition: "object-fit 0.2s ease",
+          }}
+        />
+      </Card.Section>
+      {hovered && (
+        <Group justify="space-between" mt="md" mb="xs" style={{ flexGrow: 1 }}>
+          <Text fw={500} style={{ width: "60%" }}>
             {memes.title}
-           </Text>
-          </Group>
-         )}
-        </Card>
-    )
+          </Text>
+          <Text size="sm" fw={200} style={{ width: "60%" }}>
+            {memes.description}
+          </Text>
+        </Group>
+      )}
+    </Card>
+  );
 }
 
 export default MemeItem;
